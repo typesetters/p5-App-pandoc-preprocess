25d0b160efd20af10dd62360428fe3880562f465
Daniel Bruder <daniel.bruder+git@gmail.com>
2014-01-07 16:46:05 +0100

    code blocks can appear in lists, too

--- lib/App/pandoc/preprocess.pm
+++ lib/App/pandoc/preprocess.pm
@@ -68,9 +68,9 @@ has matchers => (
   default => sub {
     +{
       begin_of_line          => qr/^/sm,
-      codeblock_begin        => qr/^~+/sm,
+      codeblock_begin        => qr/~{4,}/sm,
       codeblock_content      => qr/.*?/sm,
-      codeblock_end          => qr/^~+/sm,
+      codeblock_end          => qr/~{4,}/sm,
       format_specification   => qr/(?:dot|ditaa|rdfdot)/sm,
       random_stuff_nongreedy => qr/.*?/sm,
       possibly_spaces        => qr/\s*/sm,
@@ -95,7 +95,7 @@ sub _build_matcher {
         @{[$self->matchers->{random_stuff_nongreedy}]}
       \}
       @{[$self->matchers->{random_stuff_nongreedy}]}
-      @{[$self->matchers->{begin_of_line}]}(?<content> @{[$self->matchers->{codeblock_content}]} )
+      (?<content> @{[$self->matchers->{codeblock_content}]} )
       @{[$self->matchers->{codeblock_end}]}
     )
   /x;
